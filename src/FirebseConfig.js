11b945ea5cb68ed1541fd0d88803158e8bc8fbdd
iamayushy <ayushhuns@gmail.com>
2022-07-25 11:24:59 +0530

    added the firebase confing

---
+++ src/FirebseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQng-PBINd9J-LtNgicLUoquaMeW4Y__8",
+  authDomain: "clearbook-562b5.firebaseapp.com",
+  projectId: "clearbook-562b5",
+  storageBucket: "clearbook-562b5.appspot.com",
+  messagingSenderId: "164353852528",
+  appId: "1:164353852528:web:e1bad4eb530181dadd20b5"
+};
+
+const app = initializeApp(firebaseConfig);
